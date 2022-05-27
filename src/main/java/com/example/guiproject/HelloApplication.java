ab6bcbe7f11a388e35c9bacd87c3d3aa4a25ebe8
xmu-csnoob <395110618@qq.com>
2022-05-27 15:01:22 +0800

    Commit测试

--- src/main/java/com/example/guiproject/HelloApplication.java
+++ src/main/java/com/example/guiproject/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 450, 312);
-        stage.setTitle("Hello!");
+        stage.setTitle("Commit");
         stage.setScene(scene);
         stage.show();
     }
