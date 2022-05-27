c5d01245c3c08e823c1962407ba59cf78c0b92e5
Wenfei Wang <395110618@qq.com>
2022-05-27 14:52:30 +0800

    First Commit:Initialization of a JavaFX project

--- src/main/java/com/example/guiproject/HelloController.java
+++ src/main/java/com/example/guiproject/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.guiproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private PasswordField passwordTextField;
+
+
+    @FXML
+    private Button loginButton;
+
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    void loginButtonClicked(MouseEvent mouseEvent) {
+        if(usernameTextField.getText().equals("wwf")&&passwordTextField.getText().equals("wwf"))
+        {
+            Alert alert=new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("登录成功");
+            alert.setContentText("Login Success");
+            alert.show();
+        }
     }
 }
